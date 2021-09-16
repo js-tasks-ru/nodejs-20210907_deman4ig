8f5d770802e97e934ebbf93098c49a13b4c7bc74
Dmitry Fomin <d.fomin@webeng.xyz>
2021-09-16 18:51:43 -0400

    Add solution for module 2 (streams) task 2 (split stream)

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,36 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._reminder = '';
   }
 
   _transform(chunk, encoding, callback) {
+    let startPos = 0;
+    let finishPos = 0;
+
+    if (!chunk.includes(os.EOL)) {
+      this._reminder += chunk;
+      callback();
+      return;
+    }
+
+    if (this._reminder.length > 0) {
+      chunk = this._reminder + chunk;
+      this._reminder = '';
+    }
+
+    while (chunk.indexOf(os.EOL, startPos) !== -1) {
+      finishPos = chunk.indexOf(os.EOL, startPos);
+      this.push(chunk.slice(startPos, finishPos));
+      startPos = finishPos + 1;
+    }
+
+    this._reminder += chunk.slice(finishPos + 1);
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this._reminder);
   }
 }
 
